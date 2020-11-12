9c3923465a458d6797ec5fb23e7412237607fabe
Kiki <Kiki@Kiki-PC>
2020-11-12 22:10:40 +0700

    Upload Project

---
+++ src/pboif2/pkg10119052/latihan47/nilaimahasiswa/PBOIF210119052Latihan47NilaiMahasiswa.java
@@ -0,0 +1,24 @@
+package pboif2.pkg10119052.latihan47.nilaimahasiswa;
+
+/**
+ *
+ * NAMA   : Rizki Dwi nugraha
+ * KELAS  : IF-2
+ * NIM    : 10119052
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * nilai mahasiswa berbasis objek
+ * 
+ */
+public class PBOIF210119052Latihan47NilaiMahasiswa {
+
+    public static void main(String[] args) {
+        Rumus nilai1,nilai2,nilai3;
+        nilai1 = new Rumus(75,45,34);
+        nilai1.tampil();
+        nilai2 = new Rumus(60,80,75);
+        nilai2.tampil();
+        nilai3 = new Rumus(30,80,40);
+        nilai3.tampil();
+    }
+    
+}
